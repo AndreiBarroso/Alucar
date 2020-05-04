2bf5a751ce5e49616519f111d202fa3f21df91dd
Andrei Barroso <andrei.mesquita@globomail.com>
2020-05-04 16:36:18 -0300

    all

--- src/main/java/tcc/controlador/LoginController.java
+++ src/main/java/tcc/controlador/LoginController.java
@@ -1,22 +1,22 @@
-package tcc.controlador;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class LoginController {
-
-    @GetMapping(path = "/login")
-    public String login() {
-        return "login";
-    }
-
-    @ExceptionHandler(Exception.class)
-
-    public String handleException(Exception error) throws Exception {
-        return "redirect:/login?error=Login incorreto";
-
-    }
-
-}
+//package tcc.controlador;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class LoginController {
+//
+//    @GetMapping(path = "/login")
+//    public String login() {
+//        return "login";
+//    }
+//
+//    @ExceptionHandler(Exception.class)
+//
+//    public String handleException(Exception error) throws Exception {
+//        return "redirect:/login?error=Login incorreto";
+//
+//    }
+//
+//}
